41fbab1dc2d43334aacba1fdf6346c464db5825c
ian-hamlin <ian@ihamlin.co.uk>
2019-07-08 08:48:29 +0100

    refactor.

--- src/clean.rs
+++ src/clean.rs
@@ -5,14 +5,21 @@ pub trait Clean {
 impl Clean for String {
     fn remove_anchor(&self) -> Self {
         let mut result = String::new();
+        // Find the bits to chop.
+        let left = self.find('>');
+        let right = self.rfind('<');
 
-        if let Some(left) = self.find('>') {
-            if let Some(right) = self.rfind('<') {
+        if left.is_some() && right.is_some() {
+            // If both are some, we can enter and unwrap safely.
+            let left = left.unwrap();
+            let right = right.unwrap();
+
+            if left < right {
                 result.push_str(&self[left + 1..right]);
             }
         }
-
         if result.is_empty() {
+            // Else just keep the original input.
             result.push_str(self);
         }
 
