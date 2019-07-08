f2f3e3f5f0454ccc3ff7351864219487d1db83eb
ian-hamlin <ian@ihamlin.co.uk>
2019-07-08 09:06:10 +0100

    tests

--- src/clean.rs
+++ src/clean.rs
@@ -26,3 +26,49 @@ impl Clean for String {
         result
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn should_return_input() {
+        let input = String::from("hello world");
+        let output = input.remove_anchor();
+
+        assert_eq!("hello world".to_string(), output);
+    }
+
+    #[test]
+    fn should_return_input_if_wrong_order() {
+        let input = String::from("<hello world>");
+        let output = input.remove_anchor();
+
+        assert_eq!("<hello world>".to_string(), output);
+    }
+
+    #[test]
+    fn should_return_input_if_missing_end() {
+        let input = String::from("<a href>hello world");
+        let output = input.remove_anchor();
+
+        assert_eq!("<a href>hello world".to_string(), output);
+    }
+
+    #[test]
+    fn should_return_input_if_missing_start() {
+        let input = String::from("hello world</a>");
+        let output = input.remove_anchor();
+
+        assert_eq!("hello world</a>".to_string(), output);
+    }
+
+    #[test]
+    fn should_return_anchor_content() {
+        let input = String::from("<a href>hello world</a>");
+        let output = input.remove_anchor();
+
+        assert_eq!("hello world".to_string(), output);
+    }
+}
